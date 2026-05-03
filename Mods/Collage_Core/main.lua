a74ef8c64e996296059474a66f95e817dc04d20d
spencers145 <smartypantsspencer@gmail.com>
2026-05-02 21:43:04 -0400

    Bump Version

--- Mods/Collage_Core/main.lua
+++ Mods/Collage_Core/main.lua
@@ -234,7 +234,7 @@ function Game:main_menu(change_context)
                     n = G.UIT.T,
                     config = {
                         scale = 0.3,
-                        text = "Collage v1.4.0b",
+                        text = "Collage v1.4.0c",
                         colour = G.C.UI.TEXT_LIGHT
                     }
                 }
