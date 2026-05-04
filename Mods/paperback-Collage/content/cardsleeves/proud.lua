2b6e1877a713b4db47024d8a7d97f534ad368974
spencers145 <smartypantsspencer@gmail.com>
2026-05-04 19:20:26 -0400

    Add Proud Sleeve

--- Mods/paperback-Collage/content/cardsleeves/proud.lua
+++ Mods/paperback-Collage/content/cardsleeves/proud.lua
@@ -1,30 +1,37 @@
-if PB_UTIL.config.suits_enabled then
-  PB_UTIL.Sleeve {
-    key = 'proud',
-    deck_buff = 'b_paperback_proud',
-    atlas = 'card_sleeves_atlas',
-    pos = { x = 1, y = 0 },
-    paperback = {
-      create_crowns = true,
-      create_stars = true
-    },
-    unlocked = false,
-    unlock_condition = { deck = "b_paperback_proud", stake = "stake_green" },
+PB_UTIL.Sleeve {
+  key = 'proud',
+  deck_buff = 'b_paperback_proud',
+  atlas = 'card_sleeves_atlas',
+  pos = { x = 1, y = 0 },
 
-    apply = function(self, sleeve)
-      if self:is_buffed() then
-        -- Apply polychrome to all Aces
-        G.E_MANAGER:add_event(Event {
-          func = function()
-            for _, v in ipairs(G.playing_cards or {}) do
-              if v:get_id() == 14 then
-                v:set_edition('e_polychrome', true, true)
-              end
-            end
-            return true
+  config = {
+    consumables = {
+      'c_lovers',
+      'c_lovers',
+    }
+  },
+  unlocked = true,
+
+  loc_vars = function(self)
+    return {
+      key = self:loc_key(),
+      vars = {
+        localize { type = 'name_text', key = 'c_lovers', set = 'Tarot' },
+      }
+    }
+  end,
+
+  apply = function(self, sleeve)
+    -- Apply polychrome to all Aces
+    G.E_MANAGER:add_event(Event {
+      func = function()
+        for _, v in ipairs(G.playing_cards or {}) do
+          if v:get_id() == 14 then
+            v:set_edition('e_polychrome', true, true)
           end
-        })
+        end
+        return true
       end
-    end
-  }
-end
+    })
+  end
+}
